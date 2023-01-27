8d568a23be5906c34af2195a65f7916764b509ae
Mustafo <mnusrus@gmail.com>
2023-01-27 09:11:36 -0800

    init

---
+++ app.js
@@ -0,0 +1,13 @@
+let login = prompt('logindi tering')
+
+let password =prompt('parolni tering')
+
+
+if(login == "mustafo" && password == "1111"){
+   alert('Hush kelibsiz')
+}else{
+   alert('parol yoki login natori')
+}
+ 
+
+
